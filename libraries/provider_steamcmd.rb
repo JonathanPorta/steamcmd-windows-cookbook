96bb7728a586062d4537cf93cd6485be5d8372d9
Jonathan Porta <jonathan@jonathanPorta.com>
2015-09-05 23:12:34 -0700

    Pass the proper stuff to the constructor

--- libraries/provider_steamcmd.rb
+++ libraries/provider_steamcmd.rb
@@ -30,7 +30,8 @@ class Chef
         Chef::Log.debug "Built a powershell command that looks like: "
         Chef::Log.debug command_string
         Chef::Log.debug "======================================================="
-        p = Chef::Resource::PowershellScript.new "Installing Steam app #{ new_resource.app_id }" do
+        p = Chef::Resource::PowershellScript.new "Installing Steam app #{ new_resource.app_id }", run_context do
+          action :nothing
           code <<-EOH
             #{ command_string }
           EOH
